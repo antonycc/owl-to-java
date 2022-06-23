a07a5b7c84df6605c3bf99ee0fc252beef649f0a
antonycc <antonyccartwright@gmail.com>
2022-06-23 22:48:25 +0200

    Move to 0.1.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -74,7 +74,7 @@ afterEvaluate {
 // ./gradlew incrementVersion --versionIncrementType=PATCH
 // ./gradlew printVersion -Psnapshot=false
 group = "co.uk.polycode"
-version = "0.1.0"
+version = "0.1.1"
 
 // See: https://docs.gradle.org/current/userguide/publishing_maven.html
 // ./gradlew build publishToMavenLocal
